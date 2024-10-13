1b9b01cdcff411fb091db4df6fd9c724578aff94
Black7in <elperroblack158@gmail.com>
2024-10-13 15:32:14 -0400

    Add NPC gossip functionality to Halloween platform Lua script

--- eventos/halloween/plataformas.lua
+++ eventos/halloween/plataformas.lua
@@ -1,1 +1,17 @@
-test
+local npcEntry = 90021
+
+local function OnGossipHello(event, player, creature)
+    player:GossipClearMenu()
+    player:GossipMenuAddItem(0, "Teleportar para a próxima plataforma", 0, 1)
+    player:GossipSendMenu(1, creature, 200)
+end
+
+local function OnGossipSelect(event, player, creature, sender, intid, code, menuid)
+    if (intid == 1) then
+        local x, y, z = player:GetLocation()
+        player:Teleport(0, x, y, z + 10)
+    end
+end
+
+RegisterCreatureGossipEvent(npcEntry, 1, OnGossipHello)
+RegisterCreatureGossipEvent(npcEntry, 2, OnGossipSelect)
