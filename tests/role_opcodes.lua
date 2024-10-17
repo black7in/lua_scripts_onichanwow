0929e16df3e90755770b7be3def54c9154cd4c1a
Black7in <elperroblack158@gmail.com>
2024-10-17 01:24:38 -0400

    Update SMSG_LFG_ROLE_CHOSEN opcode in role_opcodes.lua

--- tests/role_opcodes.lua
+++ tests/role_opcodes.lua
@@ -1,13 +1,7 @@
-local SMSG_LFG_ROLE_CHOSEN = 0x2BB
+local SMSG_LFG_ROLE_CHOSEN = 0x361
 
 local function OnPacketSend(event, packet, player) 
     print("Player: " .. player:GetName())
-    local value = packet:ReadGUID()
-    --print("Read GUID: " .. packet:ReadGUID())
-    print("Read UByte: " .. packet:ReadUByte())
-    print("Read ULong: " .. packet:ReadULong())
-    print("----------------------")
-
 end
 
-RegisterPacketEvent(SMSG_LFG_ROLE_CHOSEN, 7, OnPacketSend)
+RegisterPacketEvent(SMSG_LFG_PROPOSAL_UPDATE, 7, OnPacketSend)
