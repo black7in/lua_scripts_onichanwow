d7656fa2c568756688b4671c5b42ec3a09961162
Black7in <elperroblack158@gmail.com>
2024-10-17 01:27:08 -0400

    Refactor packet logging and add packet receive event in role_opcodes.lua

--- tests/role_opcodes.lua
+++ tests/role_opcodes.lua
@@ -1,7 +1,12 @@
 local SMSG_LFG_PROPOSAL_UPDATE = 0x361
 
+local function OnPacketReceive(event, packet, player) 
+    print("Received packet: " .. SMSG_LFG_PROPOSAL_UPDATE)
+end
+
 local function OnPacketSend(event, packet, player) 
-    print("Player: " .. player:GetName())
+    print("Sent packet: " .. SMSG_LFG_PROPOSAL_UPDATE)
 end
 
 RegisterPacketEvent(SMSG_LFG_PROPOSAL_UPDATE, 7, OnPacketSend)
+RegisterPacketEvent(SMSG_LFG_PROPOSAL_UPDATE, 5, OnPacketReceive)
