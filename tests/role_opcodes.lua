670d0b30cc9b33294ee12904bcadae8b7f97dd82
Black7in <elperroblack158@gmail.com>
2024-10-17 00:47:20 -0400

    Add packet logging in role_opcodes.lua

--- tests/role_opcodes.lua
+++ tests/role_opcodes.lua
@@ -1,12 +1,12 @@
 local SMSG_LFG_ROLE_CHOSEN = 0x2BB
 
-local function OnPacketReceive(event, packet, player) 
-    print("Received packet: " .. SMSG_LFG_ROLE_CHOSEN)
-end
-
 local function OnPacketSend(event, packet, player) 
-    print("Sent packet: " .. SMSG_LFG_ROLE_CHOSEN)
+    print("Player: " .. player:GetName())
+    print("Read GUID: " .. packet:ReadGUID())
+    print("Read UByte: " .. packet:ReadUByte())
+    print("Read ULong: " .. packet:ReadULong())
+    print("----------------------")
+
 end
 
-RegisterPacketEvent(SMSG_LFG_ROLE_CHOSEN, 5, OnPacketReceive)
 RegisterPacketEvent(SMSG_LFG_ROLE_CHOSEN, 7, OnPacketSend)
