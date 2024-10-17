969c7e9cd439b959fd2ab170ea39cb9ea799d680
Black7in <elperroblack158@gmail.com>
2024-10-17 00:49:41 -0400

    Refactor packet logging in role_opcodes.lua

--- tests/role_opcodes.lua
+++ tests/role_opcodes.lua
@@ -2,7 +2,8 @@ local SMSG_LFG_ROLE_CHOSEN = 0x2BB
 
 local function OnPacketSend(event, packet, player) 
     print("Player: " .. player:GetName())
-    print("Read GUID: " .. packet:ReadGUID())
+    local value = packet:ReadGUID()
+    --print("Read GUID: " .. packet:ReadGUID())
     print("Read UByte: " .. packet:ReadUByte())
     print("Read ULong: " .. packet:ReadULong())
     print("----------------------")
