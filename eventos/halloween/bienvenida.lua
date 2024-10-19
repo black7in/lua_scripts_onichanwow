81f5c09ff0b61693e2f9c854baa58d325aac3837
Black7in <elperroblack158@gmail.com>
2024-10-19 02:02:42 -0400

    Refactor OnUpdateArea function in bienvenida.lua

---
+++ eventos/halloween/bienvenida.lua
@@ -0,0 +1,10 @@
+local function OnUpdateArea(event, player, oldArea, newArea)
+    if newArea ~= 41 then return end
+
+    local nearestCreature = player:GetNearestCreature( 10, 90022 )
+    if nearestCreature then
+        player:SendNotification("|TINTERFACE/ICONS/Ability_Creature_Cursed_05:30:30:0:0|t |CFF330033Bienvenido a la tienda Halloween|R |TINTERFACE/ICONS/Ability_Creature_Cursed_05:30:30:0:0|t")
+    end
+end
+
+RegisterPlayerEvent(47, OnUpdateArea)
