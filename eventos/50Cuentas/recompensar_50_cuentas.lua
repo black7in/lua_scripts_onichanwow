ba959d559f054209e7920817b9be2ce51fb0ac03
Black7in <elperroblack158@gmail.com>
2024-10-22 03:18:49 -0400

    Refactor .gitignore: Add eventos/50Cuentas/cuentas.data Add eventos/50Cuentas/recompensar_50_cuentas.lua

---
+++ eventos/50Cuentas/recompensar_50_cuentas.lua
@@ -0,0 +1,39 @@
+local npcEntry = 70000
+
+local archivo = "/root/server/bin/lua_scripts/eventos/50Cuentas/cuentas.data"
+local cuentas = cargarVariablesEnv(archivo)
+
+local function OnGossipHello(event, player, creature)
+    player:GossipClearMenu()
+    player:GossipMenuAddItem(0, "Reclamar mi recompensa", 1, 0)
+    -- adios
+    player:GossipMenuAddItem(0, "Adios", 2, 0)
+    player:GossipSendMenu(1, creature)
+end
+
+local function OnGossipSelect(event, player, creature, sender, intid, code)
+    if intid == 1 then
+        local estadoCuenta = cuentas[player:GetAccountName()]
+        if estadoCuenta then
+            if estadoCuenta == "reclamado" then
+                player:SendUnitSay("Ya has reclamado tu recompensa.", 0)
+            else
+                player:SendUnitSay("Felicidades, has reclamado tu recompensa.", 0)
+                player:AddItem(19429, 1)
+                cuentas[player:GetAccountName()] = "reclamado"
+                cambiarVariableEnv(archivo, player:GetAccountName(), "reclamado")
+            end
+        else
+            player:SendUnitSay("Tu cuenta no esta en la lista de las primeras 50 cuentas Registradas.", 0)
+        end
+    end
+    if intid == 2 then
+        creature:SendUnitSay("Adios", player)
+    end
+    player:GossipComplete()
+
+end
+
+RegisterCreatureGossipEvent(npcEntry, 1, OnGossipHello)
+RegisterCreatureGossipEvent(npcEntry, 2, OnGossipSelect)
+
